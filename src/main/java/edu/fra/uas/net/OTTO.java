61526ded0d0736497935667d5ba062ede0619bf8
alehmannFRA-UAS <88931807+alehmannFRA-UAS@users.noreply.github.com>
2022-04-26 16:46:20 +0200

    xcbcbyxcbn

--- src/main/java/edu/fra/uas/net/OTTO.java
+++ src/main/java/edu/fra/uas/net/OTTO.java
@@ -5,5 +5,12 @@ class OTTO {
  int ABC = 5;
  
  public OTTO() {};
+ 
+     public int d(int a, int b) {
+        if (b == 0)
+            return Integer.MAX_VALUE;
+        else
+            return a / b;
+    }
 
 }
